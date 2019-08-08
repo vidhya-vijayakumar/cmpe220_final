c1625de11dd7a9100c8b4c4cf1f196ff6ac69c73
vidhya-vijayakumar <36356627+vidhya-vijayakumar@users.noreply.github.com>
2019-08-07 17:44:53 -0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //url: 'mongodb://localhost:27017/walmart_sales'
+    //url: "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test"
+    //url: "mongodb+srv://vijayakumar:cmpe2201*@cluster0-bc9fl.mongodb.net/sensor-tag"
+    // mongodb+srv://vijayakumar:<password>@cluster0-bc9fl.mongodb.net/test?retryWrites=true&w=majority
+
+    url: "mongodb+srv://CMPE280:CMPE280@cluster0-199rm.mongodb.net/walmart_sales"
+}
